a56a3bde01afd3402c784fd6da83e1e67231007c
Tolmjoe <jayeobae@gmail.com>
2025-04-23 22:19:33 +0100

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import './stylesheets/App.css'
 function App() {
     if(localStorage.getItem("theme") === "dark"){
         document.body.classList.add("dark")
+    } else {
+        document.body.classList.remove("dark")
     }
     return(
         <Router>
